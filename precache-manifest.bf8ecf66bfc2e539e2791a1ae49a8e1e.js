913c47fe77605abddf47973f6bc4a442c260f8c3
demikhovr <demikhovr@gmail.com>
2019-01-27 13:18:20 +0300

    Updates

---
+++ precache-manifest.bf8ecf66bfc2e539e2791a1ae49a8e1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b56000388d32afb01645",
+    "url": "/react-redux-theory/static/js/runtime~main.b5600038.js"
+  },
+  {
+    "revision": "7ab6dd1fb33f31831a50",
+    "url": "/react-redux-theory/static/js/main.7ab6dd1f.chunk.js"
+  },
+  {
+    "revision": "a69269067c6eb5afa374",
+    "url": "/react-redux-theory/static/js/1.a6926906.chunk.js"
+  },
+  {
+    "revision": "7ab6dd1fb33f31831a50",
+    "url": "/react-redux-theory/static/css/main.18063d53.chunk.css"
+  },
+  {
+    "revision": "2a6ad2f1cb49a37218b758d5c3a6e127",
+    "url": "/react-redux-theory/index.html"
+  }
+];
